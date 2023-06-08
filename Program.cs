e04f7451db6cff923a7cc89fb6de22ea7599aa00
logan <logan.ragland25@gmail.com>
2023-06-08 16:17:56 -0400

    6/8/2023 4:18pm
        Fixed bugs that:
            Prevented you from rerolling textures for the same folder
            Allowed program to select the same file twice, and tried to switvh it with itseld
            Allowed program to select the temp files for switching

--- Program.cs
+++ Program.cs
@@ -26,38 +26,52 @@ class Program
                 /************************************************/
                 //Set random number and save original texture names
                 randomIndex = rnd.Next(0, dirs.Count);
+                while (randomIndex == shuffle)
+                {
+                    randomIndex = rnd.Next(0, dirs.Count);
+                }
                 original = Path.GetFileName(dirs[shuffle]);
                 transfer = Path.GetFileName(dirs[randomIndex]);
 
                 /************************************************/
                 //Print out results and variables for debugging
                 Console.WriteLine();
-                Console.WriteLine(shuffle);
+                Console.WriteLine("Position: " + shuffle);
                 Console.WriteLine();
                 
                 Console.WriteLine();
-                Console.WriteLine(original);
-                Console.WriteLine(transfer);
+                Console.WriteLine("File at Position: " + original);
+                Console.WriteLine("File to be Switched With: " + transfer);
                 Console.WriteLine();
 
-                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%");
-                Console.WriteLine(dirs[shuffle]);
-                Console.WriteLine(docPath + "\\" + temp1);
+                Console.WriteLine("%%%%%%");
+                Console.WriteLine("Path to File at Position: " + dirs[shuffle]);
+                Console.WriteLine("Path to temp1 File: " + docPath + "\\" + temp1);
                 Console.WriteLine();
                 
                 Console.WriteLine("*****");
-                Console.WriteLine(dirs[randomIndex]);
-                Console.WriteLine(docPath + "\\" + temp2);
+                Console.WriteLine("Path to File at to be Switched: " + dirs[randomIndex]);
+                Console.WriteLine("Path to temp1 File: " + docPath + "\\" + temp2);
                 Console.WriteLine();
 
                 /************************************************/
+                System.IO.File.Delete(docPath + "\\" + temp1);
+                System.IO.File.Delete(docPath + "\\" + temp2);
+
+                //Make sure selected file is not a temp file
+                if (original.Equals("temp1.png") == false || transfer.Equals("temp2.png") == false)
+                {
                 //Change both selected files to temporary names
                 System.IO.File.Move(dirs[shuffle], docPath + "\\" + temp1);
                 System.IO.File.Move(dirs[randomIndex], docPath + "\\" + temp2);
                 
                 //Change both selected files to other texture's name
                 System.IO.File.Move(docPath + "\\" + temp1, docPath + "\\" + transfer);
-                System.IO.File.Move(docPath + "\\" + temp2, docPath + "\\" + original);
+                System.IO.File.Move(docPath + "\\" + temp2, docPath + "\\" + original);  
+                }
+               
+                System.IO.File.Delete(docPath + "\\" + temp1);
+                System.IO.File.Delete(docPath + "\\" + temp2);
             }
         }
         catch (UnauthorizedAccessException ex)
